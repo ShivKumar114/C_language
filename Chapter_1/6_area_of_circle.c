4c64612ba0521e90663b77b3cbbc2f18b498d5b7
ShivKumar114 <shivswarnkar114@gmail.com>
2026-01-03 05:28:33 +0530

    area of circle

---
+++ Chapter_1/6_area_of_circle.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+ int main(){
+    int radius;
+    printf("Enter radius: ");
+    scanf("%d",&radius);
+    int area = (3.14*radius*radius);
+    printf("Area of circle is %d",area);
+    return 0;
+
+ }
