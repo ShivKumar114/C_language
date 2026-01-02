9b5f5d96a42abbedf5c5735aed9b18198e7fe7bd
ShivKumar114 <shivswarnkar114@gmail.com>
2026-01-03 05:25:54 +0530

    :simple program

---
+++ Chapter_1/1_simple_program.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main(){
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    float pi = 3.14;
+
+    printf("Number = %d\n", number);
+    printf("Star = %c\n", star);
+    printf("Age = %d\n", age);
+    printf("Pi = %.2f\n", pi);
+    return 0;
+}
+
