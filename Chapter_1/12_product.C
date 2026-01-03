2771767ac04896616336decc9b68565350c91784
ShivKumar114 <shivswarnkar114@gmail.com>
2026-01-03 14:31:49 +0530

    Product

---
+++ Chapter_1/12_product.C
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    int a;
+    printf("ENter your number: ");
+    scanf("%d",&a);
+    int b;
+    printf("ENter your number: ");
+    scanf("%d",&b);
+    int product = a*b;
+    printf("%d",product);
+    return 0;
+}
