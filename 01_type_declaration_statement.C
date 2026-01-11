0e06b874a947711ce51691daf2e9067d982b1f2a
ShivKumar114 <shivswarnkar114@gmail.com>
2026-01-11 19:07:37 +0530

    Type declaration statement

---
+++ 01_type_declaration_statement.C
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main(){
+    int a = 22;
+    int b =a;
+    int c = b*6;
+    int d = 1;
+    int e = d;
+
+    
+    printf("%d %d %d %d %d",a,b,c,d,e);
+    return 0;
+}
